b9e0be4090df8ee881742ce4cadebab79d738f57
cc_ggboy <chenweimin8@huawei.com>
2022-05-13 10:09:00 +0800

    cwm_0513_fuzztest
    Signed-off-by: cc_ggboy <chenweimin8@huawei.com>

---
+++ test/fuzztest/access_token/grantpermission_fuzzer/grantpermission_fuzzer.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "grantpermission_fuzzer.h"
+
+#include <string>
+#include <vector>
+#include <thread>
+#undef private
+#include "accesstoken_kit.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+AccessTokenID TOKENID = 1;
+
+namespace OHOS {
+    bool GrantPermissionFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        if ((data == nullptr) || (size <= 0)) {
+            return result;
+        }
+        if (size > 0) {
+            result = AccessTokenKit::GrantPermission(TOKENID, reinterpret_cast<const char*>(data), 0);
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GrantPermissionFuzzTest(data, size);
+    return 0;
+}
