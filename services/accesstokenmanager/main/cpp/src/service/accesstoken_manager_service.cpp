f2b2c228713254fa818417b50844cec49aab68b2
chennian <chennian1@huawei.com>
2022-06-12 15:47:19 +0800

    Signed-off-by:chennian<chennian1@huawei.com>
    Signed-off-by: chennian <chennian1@huawei.com>

--- services/accesstokenmanager/main/cpp/src/service/accesstoken_manager_service.cpp
+++ services/accesstokenmanager/main/cpp/src/service/accesstoken_manager_service.cpp
@@ -29,7 +29,7 @@
 #include "native_token_receptor.h"
 #include "permission_list_state.h"
 #include "permission_manager.h"
-#include "privacy_kit.h"
+#include "permission_record_manager.h"
 
 namespace OHOS {
 namespace Security {
@@ -195,7 +195,7 @@ int AccessTokenManagerService::ClearUserGrantedPermissionState(AccessTokenID tok
     ACCESSTOKEN_LOG_INFO(LABEL, "called, tokenID: 0x%{public}x", tokenID);
     PermissionManager::GetInstance().ClearUserGrantedPermissionState(tokenID);
     AccessTokenInfoManager::GetInstance().RefreshTokenInfoIfNeeded();
-    PrivacyKit::RemovePermissionUsedRecords(tokenID, "");
+    PermissionRecordManager::GetInstance().RemovePermissionUsedRecords(tokenID, "");
     return RET_SUCCESS;
 }
 
@@ -216,7 +216,7 @@ AccessTokenIDEx AccessTokenManagerService::AllocHapToken(const HapInfoParcel& in
 int AccessTokenManagerService::DeleteToken(AccessTokenID tokenID)
 {
     ACCESSTOKEN_LOG_INFO(LABEL, "called, tokenID: 0x%{public}x", tokenID);
-    PrivacyKit::RemovePermissionUsedRecords(tokenID, "");
+    PermissionRecordManager::GetInstance().RemovePermissionUsedRecords(tokenID, "");
     // only support hap token deletion
     return AccessTokenInfoManager::GetInstance().RemoveHapTokenInfo(tokenID);
 }
