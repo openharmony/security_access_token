81ee43541476207f6b85f323ec9dad14062ab8e2
cc_ggboy <chenweimin8@huawei.com>
2022-07-14 20:53:45 +0800

    头文件整改
    Signed-off-by: cc_ggboy <chenweimin8@huawei.com>

--- interfaces/innerkits/nativetoken/src/nativetoken.c
+++ interfaces/innerkits/nativetoken/src/nativetoken.c
@@ -20,12 +20,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bits/errno.h"
 #include "bits/fcntl.h"
 #include "errno.h"
 #include "fcntl.h"
 #include "securec.h"
-#include <string.h>
 #include "sys/stat.h"
 #include "unistd.h"
 #include "nativetoken_log.h"
