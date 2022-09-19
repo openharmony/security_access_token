0e08ee60b5cad07dfa91ae4120a8d24817fe3934
zhouyan <zhouyan128@huawei.com>
2022-09-19 11:32:53 +0800

    添加HapPolicyParcel中permList的分支覆盖率
    Signed-off-by: zhouyan <zhouyan128@huawei.com>
    Change-Id: I935957801d7b3fbcc6a12a06b2b681fadd5330d1

--- frameworks/accesstoken/test/unittest/src/hap_policy_parcel_test.cpp
+++ frameworks/accesstoken/test/unittest/src/hap_policy_parcel_test.cpp
@@ -30,6 +30,42 @@ namespace AccessToken {
 namespace {
 static const std::string TEST_PERMISSION_NAME_ALPHA = "ohos.permission.ALPHA";
 static const std::string TEST_PERMISSION_NAME_BETA = "ohos.permission.BETA";
+
+PermissionDef g_permDefAlpha = {
+    .permissionName = TEST_PERMISSION_NAME_ALPHA,
+    .bundleName = "accesstoken_test",
+    .grantMode = 1,
+    .availableLevel = APL_NORMAL,
+    .label = "label",
+    .labelId = 1,
+    .description = "annoying",
+    .descriptionId = 1
+};
+PermissionDef g_permDefBeta = {
+    .permissionName = TEST_PERMISSION_NAME_BETA,
+    .bundleName = "accesstoken_test",
+    .grantMode = 1,
+    .availableLevel = APL_NORMAL,
+    .label = "label",
+    .labelId = 1,
+    .description = "so trouble",
+    .descriptionId = 1
+};
+
+PermissionStateFull g_permStatAlpha = {
+    .permissionName = TEST_PERMISSION_NAME_ALPHA,
+    .isGeneral = true,
+    .resDeviceID = {"device"},
+    .grantStatus = {PermissionState::PERMISSION_DENIED},
+    .grantFlags = {PermissionFlag::PERMISSION_USER_SET}
+};
+PermissionStateFull g_permStatBeta = {
+    .permissionName = TEST_PERMISSION_NAME_BETA,
+    .isGeneral = true,
+    .resDeviceID = {"device"},
+    .grantStatus = {PermissionState::PERMISSION_GRANTED},
+    .grantFlags = {PermissionFlag::PERMISSION_SYSTEM_FIXED}
+};
 }
 class HapPolicyParcelTest : public testing::Test  {
 public:
@@ -52,25 +88,15 @@ void HapPolicyParcelTest::TearDown(void) {}
  */
 HWTEST_F(HapPolicyParcelTest, HapPolicyParcel001, TestSize.Level1)
 {
-    PermissionStateFull permStatAlpha = {
-        .permissionName = TEST_PERMISSION_NAME_ALPHA,
-        .isGeneral = true,
-        .resDeviceID = {"device"},
-        .grantStatus = {PermissionState::PERMISSION_DENIED},
-        .grantFlags = {PermissionFlag::PERMISSION_USER_SET}
-    };
-    PermissionStateFull permStatBeta = {
-        .permissionName = TEST_PERMISSION_NAME_BETA,
-        .isGeneral = true,
-        .resDeviceID = {"device"},
-        .grantStatus = {PermissionState::PERMISSION_GRANTED},
-        .grantFlags = {PermissionFlag::PERMISSION_SYSTEM_FIXED}
-    };
     HapPolicyParcel hapPolicyParcel;
-    hapPolicyParcel.hapPolicyParameter.apl = ATokenAplEnum::APL_NORMAL; 
-    hapPolicyParcel.hapPolicyParameter.domain = "test.domain";   
-    hapPolicyParcel.hapPolicyParameter.permStateList.emplace_back(permStatAlpha);
-    hapPolicyParcel.hapPolicyParameter.permStateList.emplace_back(permStatBeta);
+
+    hapPolicyParcel.hapPolicyParameter.apl = ATokenAplEnum::APL_NORMAL;
+    hapPolicyParcel.hapPolicyParameter.domain = "test.domain";
+    hapPolicyParcel.hapPolicyParameter.permList.emplace_back(g_permDefAlpha);
+    hapPolicyParcel.hapPolicyParameter.permList.emplace_back(g_permDefBeta);
+    hapPolicyParcel.hapPolicyParameter.permStateList.emplace_back(g_permStatAlpha);
+    hapPolicyParcel.hapPolicyParameter.permStateList.emplace_back(g_permStatBeta);
+
     Parcel parcel;
     EXPECT_EQ(true, hapPolicyParcel.Marshalling(parcel));
 
@@ -78,15 +104,41 @@ HWTEST_F(HapPolicyParcelTest, HapPolicyParcel001, TestSize.Level1)
     EXPECT_EQ(true, readedData != nullptr);
 
     EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.apl, readedData->hapPolicyParameter.apl);
-    EXPECT_EQ(true, hapPolicyParcel.hapPolicyParameter.domain == readedData->hapPolicyParameter.domain);
-    EXPECT_EQ(true, hapPolicyParcel.hapPolicyParameter.permStateList.size() == readedData->hapPolicyParameter.permStateList.size());
+    EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.domain, readedData->hapPolicyParameter.domain);
+    EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList.size(), readedData->hapPolicyParameter.permList.size());
+    EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList.size(),
+        readedData->hapPolicyParameter.permStateList.size());
+
+    for(int32_t i = 0; i < hapPolicyParcel.hapPolicyParameter.permList.size(); i++) {
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].permissionName,
+            readedData->hapPolicyParameter.permList[i].permissionName);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].bundleName,
+            readedData->hapPolicyParameter.permList[i].bundleName);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].grantMode,
+            readedData->hapPolicyParameter.permList[i].grantMode);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].availableLevel,
+            readedData->hapPolicyParameter.permList[i].availableLevel);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].label,
+            readedData->hapPolicyParameter.permList[i].label);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].labelId,
+            readedData->hapPolicyParameter.permList[i].labelId);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].description,
+            readedData->hapPolicyParameter.permList[i].description);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permList[i].descriptionId,
+            readedData->hapPolicyParameter.permList[i].descriptionId);
+    }
 
     for(int32_t i = 0; i < hapPolicyParcel.hapPolicyParameter.permStateList.size(); i++) {
-        EXPECT_EQ(true, hapPolicyParcel.hapPolicyParameter.permStateList[i].permissionName == readedData->hapPolicyParameter.permStateList[i].permissionName);
-        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList[i].isGeneral, readedData->hapPolicyParameter.permStateList[i].isGeneral);
-        EXPECT_EQ(true, hapPolicyParcel.hapPolicyParameter.permStateList[i].resDeviceID == readedData->hapPolicyParameter.permStateList[i].resDeviceID);
-        EXPECT_EQ(true, hapPolicyParcel.hapPolicyParameter.permStateList[i].grantStatus == readedData->hapPolicyParameter.permStateList[i].grantStatus);
-        EXPECT_EQ(true, hapPolicyParcel.hapPolicyParameter.permStateList[i].grantFlags == readedData->hapPolicyParameter.permStateList[i].grantFlags);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList[i].permissionName,
+            readedData->hapPolicyParameter.permStateList[i].permissionName);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList[i].isGeneral,
+            readedData->hapPolicyParameter.permStateList[i].isGeneral);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList[i].resDeviceID,
+            readedData->hapPolicyParameter.permStateList[i].resDeviceID);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList[i].grantStatus,
+            readedData->hapPolicyParameter.permStateList[i].grantStatus);
+        EXPECT_EQ(hapPolicyParcel.hapPolicyParameter.permStateList[i].grantFlags,
+            readedData->hapPolicyParameter.permStateList[i].grantFlags);
     }
 }
 }
