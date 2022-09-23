6ce4af91edcd7dd30534897d9aa99fb5a8e3e629
zhouyan <zhouyan128@huawei.com>
2022-09-23 19:50:17 +0800

    添加libaccesstoken_framework_test测试套
    Signed-off-by: zhouyan <zhouyan128@huawei.com>
    Change-Id: I3e0288a1d6aa9644f72d055d12d9151f1c9366e7

---
+++ frameworks/test/unittest/common_test.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <string>
+
+#include "constant_common.h"
+
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Security {
+namespace AccessToken {
+class CommonTest : public testing::Test  {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+
+    void SetUp();
+    void TearDown();
+};
+
+void CommonTest::SetUpTestCase() {}
+void CommonTest::TearDownTestCase() {}
+void CommonTest::SetUp() {}
+void CommonTest::TearDown() {}
+
+/**
+ * @tc.name: EncryptDevId001
+ * @tc.desc: test EncryptDevId001 function.
+ * @tc.type: FUNC
+ * @tc.require: issueI5RUCC
+ */
+HWTEST_F(CommonTest, EncryptDevId001, TestSize.Level1)
+{
+    std::string res;
+    res = ConstantCommon::EncryptDevId("");
+    EXPECT_EQ(res, "");
+
+    res = ConstantCommon::EncryptDevId("12345");
+    EXPECT_EQ(res, "1*******");
+
+    res = ConstantCommon::EncryptDevId("123454321");
+    EXPECT_EQ(res, "1234****4321");
+}
+} // namespace AccessToken
+} // namespace Security
+} // namespace OHOS
