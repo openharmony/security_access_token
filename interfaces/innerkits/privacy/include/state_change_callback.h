31ac0e433f147039955ded3b7336621c8b287f9b
y1585740638 <yulei99@huawei.com>
2022-10-17 21:25:34 +0800

    相机状态回调
    Signed-off-by: y1585740638 <yulei99@huawei.com>
    Change-Id: I18ed83a047f0f0ea8ab1fd0f5ac8d9b3d4c2c3da

---
+++ interfaces/innerkits/privacy/include/state_change_callback.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef STATE_CHANGE_CALLBACK_H
+#define STATE_CHANGE_CALLBACK_H
+
+#include "state_change_callback_stub.h"
+#include "state_customized_cbk.h"
+
+namespace OHOS {
+namespace Security {
+namespace AccessToken {
+class StateChangeCallback : public StateChangeCallbackStub {
+public:
+    StateChangeCallback(const std::shared_ptr<StateCustomizedCbk> &subscriber);
+    ~StateChangeCallback() override;
+
+    void CameraStateChangeNotice(AccessTokenID tokenId, bool isShowing) override;
+
+    void Stop();
+
+private:
+    std::shared_ptr<StateCustomizedCbk> customizedCallback_;
+};
+} // namespace AccessToken
+} // namespace Security
+} // namespace OHOS
+
+#endif  // STATE_CHANGE_CALLBACK_H
---
+++ interfaces/innerkits/privacy/include/state_customized_cbk.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef STATE_CUSTOMIZED_CBK_H
+#define STATE_CUSTOMIZED_CBK_H
+
+#include "access_token.h"
+
+namespace OHOS {
+namespace Security {
+namespace AccessToken {
+class StateCustomizedCbk {
+public:
+    StateCustomizedCbk();
+    virtual ~StateCustomizedCbk();
+
+    virtual void CameraStateChangeNotice(AccessTokenID tokenId, bool isShowing) = 0;
+};
+} // namespace AccessToken
+} // namespace Security
+} // namespace OHOS
+
+#endif // STATE_CUSTOMIZED_CBK_H
