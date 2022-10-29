cdb53da841a7c435f32d8bc3d2b5eb991a7ed97e
zhouyan <zhouyan128@huawei.com>
2022-10-29 11:31:58 +0800

    隐私对外接口添加FUZZ用例3
    Signed-off-by: zhouyan <zhouyan128@huawei.com>
    Change-Id: Ie0aba31b8affd5cbb3f0964f411f52ed9240f802

---
+++ test/fuzztest/access_token/unregisterpermactivestatuscallback_fuzzer/unregisterpermactivestatuscallback_fuzzer.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "unregisterpermactivestatuscallback_fuzzer.h"
+
+#include <iostream>
+#include <thread>
+#include <string>
+#include <vector>
+#undef private
+#include "privacy_kit.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+
+class UnRegisterActiveFuzzTest : public PermActiveStatusCustomizedCbk {
+public:
+    explicit UnRegisterActiveFuzzTest(const std::vector<std::string> &permList)
+        : PermActiveStatusCustomizedCbk(permList)
+    {}
+
+    ~UnRegisterActiveFuzzTest()
+    {}
+
+    virtual void ActiveStatusChangeCallback(ActiveChangeResponse& result)
+    {
+        type_ = result.type;
+        return;
+    }
+
+    ActiveChangeType type_ = PERM_INACTIVE;
+};
+
+namespace OHOS {
+    bool UnRegisterPermActiveStatusCallbackFuzzTest(const uint8_t* data, size_t size)
+    {
+        int32_t result = RET_FAILED;
+        if ((data == nullptr) || (size <= 0)) {
+            return result != RET_FAILED;
+        }
+        if (size > 0) {
+            std::string testName(reinterpret_cast<const char*>(data), size);
+            std::vector<std::string> permList = {testName};
+            auto callback = std::make_shared<UnRegisterActiveFuzzTest>(permList);
+            callback->type_ = PERM_INACTIVE;
+
+            result = PrivacyKit::UnRegisterPermActiveStatusCallback(callback);
+        }
+        return result == RET_SUCCESS;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::UnRegisterPermActiveStatusCallbackFuzzTest(data, size);
+    return 0;
+}
