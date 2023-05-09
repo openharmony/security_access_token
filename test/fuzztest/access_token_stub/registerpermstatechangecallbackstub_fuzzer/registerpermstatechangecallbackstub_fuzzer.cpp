f5d8f0123210eff3e262d2ca1865c6b4c0294b57
wuliushuan <wuliushuan.30044128@huawei.com>
2023-05-09 08:44:31 +0000

    AT service add FUGetTokenTypeStubFuzzTest,GrantPermissionStubFuzzTest,RegisterPermStateChangeCallbackStubFuzzTest,ReloadNativeTokenInfoStubFuzzTest,RevokePermissionStubFuzzTest)
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: I9a09a18ed8044d2f495faaa1354e5e8c43b28af6

---
+++ test/fuzztest/access_token_stub/registerpermstatechangecallbackstub_fuzzer/registerpermstatechangecallbackstub_fuzzer.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "registerpermstatechangecallbackstub_fuzzer.h"
+
+#include <string>
+#include <thread>
+#include <vector>
+#undef private
+#include "accesstoken_manager_client.h"
+#include "accesstoken_manager_service.h"
+#include "i_accesstoken_manager.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+
+class CbCustomizeTest2 : public PermStateChangeCallbackCustomize {
+public:
+    explicit CbCustomizeTest2(const PermStateChangeScope &scopeInfo)
+        : PermStateChangeCallbackCustomize(scopeInfo)
+    {
+    }
+
+    ~CbCustomizeTest2()
+    {}
+
+    virtual void PermStateChangeCallback(PermStateChangeInfo& result)
+    {
+        ready_ = true;
+    }
+
+    bool ready_ = false;
+};
+
+namespace OHOS {
+    bool RegisterPermStateChangeCallbackStubFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+
+        AccessTokenID TOKENID = static_cast<AccessTokenID>(size);
+        std::string testName(reinterpret_cast<const char *>(data), size);
+
+        PermStateChangeScope scopeInfo;
+        scopeInfo.permList = { testName };
+        scopeInfo.tokenIDs = { TOKENID };
+        auto callbackPtr = std::make_shared<CbCustomizeTest2>(scopeInfo);
+
+        PermStateChangeScopeParcel scopeParcel;
+        scopeParcel.scope = scopeInfo;
+
+        sptr<PermissionStateChangeCallback> callback;
+        callback = new (std::nothrow) PermissionStateChangeCallback(callbackPtr);
+
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IAccessTokenManager::GetDescriptor());
+        if (!datas.WriteParcelable(&scopeParcel)) {
+                return false;
+            }
+            if (!datas.WriteRemoteObject(callback->AsObject())) {
+                return false;
+            }
+
+        uint32_t code = static_cast<uint32_t>(
+            IAccessTokenManager::InterfaceCode::REGISTER_PERM_STATE_CHANGE_CALLBACK);
+
+        MessageParcel reply;
+        MessageOption option;
+        DelayedSingleton<AccessTokenManagerService>::GetInstance()->OnRemoteRequest(code, datas, reply, option);
+
+        return true;
+    }
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::RegisterPermStateChangeCallbackStubFuzzTest(data, size);
+    return 0;
+}
