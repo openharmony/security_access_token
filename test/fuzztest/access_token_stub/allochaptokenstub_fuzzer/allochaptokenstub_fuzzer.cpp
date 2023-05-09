bdae64fb9ff4a97b8fbc6060997d78860314c942
wuliushuan <wuliushuan.30044128@huawei.com>
2023-05-09 03:39:07 +0000

    AT service FUZZ test(AddPermissionUsedRecordStubFuzzTest,AllocHapTokenStubFuzzTest,AllocLocalTokenIDStubFuzzTest,CheckNativeDCapStubFuzzTesClearUserGrantedPermissionStateStubFuzzTest)
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: I030ca23bbe7bf40a7ddba09839eda9707a393837

---
+++ test/fuzztest/access_token_stub/allochaptokenstub_fuzzer/allochaptokenstub_fuzzer.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "allochaptokenstub_fuzzer.h"
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#undef private
+#include "accesstoken_manager_service.h"
+#include "hap_info_parcel.h"
+#include "i_accesstoken_manager.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+
+namespace OHOS {
+    bool AllocHapTokenStubFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+
+        std::string testName(reinterpret_cast<const char *>(data), size);
+
+        PermissionDef TestPermDef = {
+            .permissionName = testName,
+            .bundleName = testName,
+            .grantMode = 1,
+            .availableLevel = APL_NORMAL,
+            .label = testName,
+            .labelId = 1,
+            .description = testName,
+            .descriptionId = 1};
+        PermissionStateFull TestState = {
+            .permissionName = testName,
+            .isGeneral = true,
+            .resDeviceID = {testName},
+            .grantStatus = {PermissionState::PERMISSION_GRANTED},
+            .grantFlags = {1},
+        };
+        HapInfoParams TestInfoParms = {
+            .userID = 1,
+            .bundleName = testName,
+            .instIndex = 0,
+            .appIDDesc = testName};
+        HapPolicyParams TestPolicyPrams = {
+            .apl = APL_NORMAL,
+            .domain = testName,
+            .permList = {TestPermDef},
+            .permStateList = {TestState}};
+
+        HapInfoParcel hapInfoParcel;
+        HapPolicyParcel hapPolicyParcel;
+        hapInfoParcel.hapInfoParameter = TestInfoParms;
+        hapPolicyParcel.hapPolicyParameter = TestPolicyPrams;
+
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IAccessTokenManager::GetDescriptor());
+        if (!datas.WriteParcelable(&hapInfoParcel)) {
+            return false;
+        }
+        if (!datas.WriteParcelable(&hapPolicyParcel)) {
+            return false;
+        }
+
+        uint32_t code = static_cast<uint32_t>(
+            IAccessTokenManager::InterfaceCode::ALLOC_TOKEN_HAP);
+
+        MessageParcel reply;
+        MessageOption option;
+        DelayedSingleton<AccessTokenManagerService>::GetInstance()->OnRemoteRequest(code, datas, reply, option);
+
+        return true;
+    }
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::AllocHapTokenStubFuzzTest(data, size);
+    return 0;
+}
