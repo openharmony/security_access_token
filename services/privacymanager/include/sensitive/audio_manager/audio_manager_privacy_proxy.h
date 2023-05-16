0496f9d8bbf21afe5e8ebebb7b7311964244bf84
zhouyan <zhouyan160@huawei.com>
2023-05-16 12:38:34 +0800

    fixed 373eae1 from https://gitee.com/yydwfzysy/security_access_token/pulls/1174 audio_framework交易码错位tdd问题修复
    Signed-off-by: zhouyan <zhouyan160@huawei.com>
    Change-Id: Id96fcb35f84429afe99ec395b427a8a475ae0be4

--- services/privacymanager/include/sensitive/audio_manager/audio_manager_privacy_proxy.h
+++ services/privacymanager/include/sensitive/audio_manager/audio_manager_privacy_proxy.h
@@ -49,7 +49,7 @@ public:
     enum AudioPolicyCommand {
         SET_MICROPHONE_MUTE = 15,
         IS_MICROPHONE_MUTE = 17,
-        SET_MIC_STATE_CHANGE_CALLBACK = 58,
+        SET_MIC_STATE_CHANGE_CALLBACK = 59,
     };
 };
 
