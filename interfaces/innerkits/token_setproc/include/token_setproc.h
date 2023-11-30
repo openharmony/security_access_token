6c8e95622f91a720d82e4f363558dd2c0994dfa2
chennian <chennian1@huawei.com>
2023-11-30 22:59:51 +0800

    Add permission cache to kernel
    Signed-off-by: chennian <chennian1@huawei.com>

--- interfaces/innerkits/token_setproc/include/token_setproc.h
+++ interfaces/innerkits/token_setproc/include/token_setproc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,11 +15,15 @@
 
 #ifndef TOKEN_SETPROC_H
 #define TOKEN_SETPROC_H
+#include<stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+#define ACCESS_TOKEN_OK 0
+#define ACCESS_TOKEN_PARAM_INVALID (1)
+#define ACCESS_TOKEN_OPEN_ERROR (2)
 
 uint64_t GetSelfTokenID();
 
@@ -28,6 +32,11 @@ int SetSelfTokenID(uint64_t tokenID);
 uint64_t GetFirstCallerTokenID();
 
 int SetFirstCallerTokenID(uint64_t tokenID);
+
+int32_t AddPermissionToKernel(uint32_t tokenID, uint32_t* opCodeList, int32_t* statusList, uint32_t size);
+int32_t RemovePermissionFromKernel(uint32_t tokenID);
+int32_t SetPermissionToKernel(uint32_t tokenID, int32_t opCode, bool status);
+bool GetPermissionFromKernel(uint32_t tokenID, int32_t opCode);
 
 #ifdef __cplusplus
 }
