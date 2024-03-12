ca5fead1b41eab698bd8273cbff0de239038ee6a
zhouyan <zhouyan160@huawei.com>
2024-03-12 17:16:55 +0800

    添加GetPermissionUsedTypeInfos接口fuzz用例
    Signed-off-by: zhouyan <zhouyan160@huawei.com>
    Change-Id: Ib0274fb0a2dd672b9b3be9d477e5dbb464acb641

---
+++ test/fuzztest/services/privacy/getpermissionusedtypeinfosstub_fuzzer/getpermissionusedtypeinfosstub_fuzzer.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getpermissionusedtypeinfosstub_fuzzer.h"
+
+#include <string>
+#include <thread>
+#include <vector>
+#undef private
+#include "i_privacy_manager.h"
+#include "privacy_manager_service.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+
+namespace OHOS {
+    bool GetPermissionUsedTypeInfosStubFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+
+        std::string testName(reinterpret_cast<const char*>(data), size);
+
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IPrivacyManager::GetDescriptor());
+
+        if (!datas.WriteUint32(static_cast<AccessTokenID>(size))) {
+            return false;
+        }
+        if (!datas.WriteString(testName)) {
+            return false;
+        }
+
+        uint32_t code = static_cast<uint32_t>(PrivacyInterfaceCode::GET_PERMISSION_USED_TYPE_INFOS);
+
+        MessageParcel reply;
+        MessageOption option;
+        DelayedSingleton<PrivacyManagerService>::GetInstance()->OnRemoteRequest(code, datas, reply, option);
+
+        return true;
+    }
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetPermissionUsedTypeInfosStubFuzzTest(data, size);
+    return 0;
+}
