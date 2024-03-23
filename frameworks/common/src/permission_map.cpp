47876b69d29f9be518fea771a02ec620a232511a
chenshuo <chenshuo42@huawei.com>
2024-03-23 14:43:47 +0800

    add DP permission
    Signed-off-by: chenshuo <chenshuo42@huawei.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -353,6 +353,8 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.ACCESS_DYNAMIC_ICON",                 false},
     {"ohos.permission.CHANGE_BUNDLE_UNINSTALL_STATE",       false},
     {"ohos.permission.MONITOR_DEVICE_NETWORK_STATE",        false},
+    {"ohos.permission.SYNC_PROFILE_DP",                     false},
+    {"ohos.permission.ACCESS_SERVICE_DP",                   false},
 };
 
 bool TransferPermissionToOpcode(const std::string& permission, uint32_t& opCode)
