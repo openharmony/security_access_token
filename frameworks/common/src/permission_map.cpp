e668f149e1101ec3f13fb988374357f619ac7244
kw <wangyuqi30@huawei.com>
2024-04-21 14:47:49 +0800

    add ohos.permission.CAST_AUDIO_OUTPUT
    Signed-off-by: kw <wangyuqi30@huawei.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -385,6 +385,7 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.ACCESS_LOCAL_BACKUP", false},
     {"ohos.permission.ACCESS_SYSTEM_APP_CERT",              false},
     {"ohos.permission.ACCESS_USER_TRUSTED_CERT",            false},
+    {"ohos.permission.CAST_AUDIO_OUTPUT",                   false},
 };
 
 bool TransferPermissionToOpcode(const std::string& permission, uint32_t& opCode)
