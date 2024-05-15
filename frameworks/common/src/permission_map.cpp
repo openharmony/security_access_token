342908f19ec401aa88fa35f1791bef587032cb4e
邹友松 <zouyousong1@h-partners.com>
2024-05-15 06:40:39 +0000

    新增跨设备访问升级接口权限
    Signed-off-by: 邹友松 <zouyousong1@h-partners.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -115,6 +115,7 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.UPDATE_SYSTEM",           false},
     {"ohos.permission.FACTORY_RESET",           false},
     {"ohos.permission.UPDATE_MIGRATE",          false},
+    {"ohos.permission.ASSIST_DEVICE_UPDATE",    false},
     {"ohos.permission.GRANT_SENSITIVE_PERMISSIONS", false},
     {"ohos.permission.REVOKE_SENSITIVE_PERMISSIONS", false},
     {"ohos.permission.GET_SENSITIVE_PERMISSIONS", false},
