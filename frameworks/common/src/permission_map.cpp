66462245daed618429165cb97cfac3148fae67d2
158******57 <qb86880301@gmail.com>
2024-05-23 23:29:34 +0800

    新增ohos.permission.ACCESS_FILE_CONTENT_SHARE权限
    Signed-off-by: 158******57 <qb86880301@gmail.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -411,6 +411,7 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.MANAGE_SETTINGS",                     false},
     {"ohos.permission.ACCESS_DEVICE_COLLABORATION_PRIVATE_ABILITY",                     false},
     {"ohos.permission.ACCESS_RINGTONE_RESOURCE",            false},
+    {"ohos.permission.ACCESS_FILE_CONTENT_SHARE",           false},
 };
 
 bool TransferPermissionToOpcode(const std::string& permission, uint32_t& opCode)
