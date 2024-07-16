82d663205b4fb64652a0fe684f0394573e4cd7e6
zhouyan <zhouyan160@huawei.com>
2024-07-16 20:12:25 +0800

    添加GetNativeTokenName接口fuzz用例
    Signed-off-by: zhouyan <zhouyan160@huawei.com>
    Change-Id: I64312aef339215ee63dd62a94f725de475d50528

---
+++ test/fuzztest/innerkits/accesstoken/getnativetokenname_fuzzer/getnativetokenname_fuzzer.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getnativetokenname_fuzzer.h"
+
+#include <iostream>
+#include <thread>
+#include <string>
+#include <vector>
+#include "accesstoken_fuzzdata.h"
+#undef private
+#include "accesstoken_kit.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+
+namespace OHOS {
+    bool GetNativeTokenNameFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+
+        AccessTokenFuzzData fuzzData(data, size);
+        string name;
+        int32_t result = AccessTokenKit::GetNativeTokenName(fuzzData.GetData<AccessTokenID>(), name);
+
+        return result == RET_SUCCESS;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetNativeTokenNameFuzzTest(data, size);
+    return 0;
+}
+
