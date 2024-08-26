d4d3cb444f452f844733015a2fec0beb92172e8a
wanganjie <wanganjie1@huawei.com>
2024-08-26 15:05:53 +0800

    新增权限允许模态UIExtension不隐藏不安全窗口
    Signed-off-by: wanganjie <wanganjie1@huawei.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -485,6 +485,7 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.GET_FILE_THUMBNAIL",                  false},
     {"ohos.permission.PUBLISH_DISPLAY_ROTATION_EVENT",                      false},
     {"ohos.permission.PUBLISH_CAST_PLUGGED_EVENT",                  false},
+    {"ohos.permission.ALLOW_SHOW_NON_SECURE_WINDOWS",       false},
 };
 
 bool TransferPermissionToOpcode(const std::string& permission, uint32_t& opCode)
