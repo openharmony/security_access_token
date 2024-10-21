435e01f8e0e64d949bc8e225c704541197a8254e
Cathy <houxi3@huawei.com>
2024-10-21 11:37:13 +0000

    update services/common/app_manager/src/process_data.cpp.
    Signed-off-by: Cathy <houxi3@huawei.com>

--- services/common/app_manager/src/process_data.cpp
+++ services/common/app_manager/src/process_data.cpp
@@ -28,7 +28,7 @@ bool ProcessData::Marshalling(Parcel &parcel) const
         parcel.WriteInt32(static_cast<int32_t>(processType)) && parcel.WriteInt32(extensionType)
         && parcel.WriteInt32(renderUid) && parcel.WriteUint32(accessTokenId) &&
         parcel.WriteBool(isTestMode) && parcel.WriteInt32(exitReason) && parcel.WriteString(exitMsg) &&
-        parcel.WriteInt32(childUid) && parcel.WriteBool(isPreload)));
+        parcel.WriteInt32(childUid) && parcel.WriteBool(isPreload));
 }
 
 bool ProcessData::ReadFromParcel(Parcel &parcel)
