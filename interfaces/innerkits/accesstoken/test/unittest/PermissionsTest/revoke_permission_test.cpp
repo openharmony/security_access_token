44646c5bbbb76257d587b03f4a6d9875d33cb9b6
丁巧巧 <dingqiaoqiao@huawei.com>
2024-11-21 12:07:33 +0000

    update interfaces/innerkits/accesstoken/test/unittest/PermissionsTest/revoke_permission_test.cpp.
    Signed-off-by: 丁巧巧 <dingqiaoqiao@huawei.com>

--- interfaces/innerkits/accesstoken/test/unittest/PermissionsTest/revoke_permission_test.cpp
+++ interfaces/innerkits/accesstoken/test/unittest/PermissionsTest/revoke_permission_test.cpp
@@ -65,7 +65,8 @@ void RevokePermissionTest::SetUpTestCase()
                                             g_infoManagerTestSystemInfoParms.instIndex);
     AccessTokenKit::DeleteToken(tokenID);
 
-    AccessTokenIDEx tokenIdEx = AccessTokenKit::AllocHapToken(g_infoManagerTestSystemInfoParms, TestCommon::GetTestPolicyParams());
+    AccessTokenIDEx tokenIdEx = AccessTokenKit::AllocHapToken(g_infoManagerTestSystemInfoParms,
+                                                              TestCommon::GetTestPolicyParams());
     SetSelfTokenID(tokenIdEx.tokenIDEx);
 }
 
