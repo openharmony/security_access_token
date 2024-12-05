d18d89fe632cf8a63a1e9078c0738ed46ba313a0
wanc361 <bluesky7417@126.com>
2024-12-05 16:05:01 +0800

    add GET_ANIM_POLICY permission
    Signed-off-by: wanc361 <bluesky7417@126.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -565,6 +565,7 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.kernel.ALLOW_WRITABLE_CODE_MEMORY", false},
     {"ohos.permission.MANAGE_UDMF_APP_SHARE_OPTION",        false},
     {"ohos.permission.MANAGE_PASTEBOARD_APP_SHARE_OPTION",  false},
+    {"ohos.permission.GET_ANIM_POLICY",                     false},
 };
 
 bool TransferPermissionToOpcode(const std::string& permission, uint32_t& opCode)
