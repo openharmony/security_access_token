6f92e006a8624cbac454175dc906ca83326da172
xuedong <xuedong@huawei.com>
2024-12-07 15:04:51 +0800

    fix: add experience hbm brightness level
    Signed-off-by: xuedong <xuedong@huawei.com>

--- frameworks/common/src/permission_map.cpp
+++ frameworks/common/src/permission_map.cpp
@@ -519,6 +519,7 @@ const static std::vector<std::pair<std::string, bool>> g_permMap = {
     {"ohos.permission.INSTALL_INTERNALTESTING_BUNDLE",     false},
     {"ohos.permission.MANAGE_UDMF_APP_SHARE_OPTION",        false},
     {"ohos.permission.MANAGE_PASTEBOARD_APP_SHARE_OPTION",  false},
+    {"ohos.permission.ENABLE_EXPERIENCE_HBM",               false},
     {"ohos.permission.kernel.DISABLE_CODE_MEMORY_PROTECTION", false},
     {"ohos.permission.kernel.ALLOW_WRITABLE_CODE_MEMORY",   false},
     {"ohos.permission.kernel.ALLOW_EXECUTABLE_FORT_MEMORY", false},
