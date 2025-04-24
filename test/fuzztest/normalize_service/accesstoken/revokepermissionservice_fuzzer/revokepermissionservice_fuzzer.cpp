85cd5db80e7d9ea555330dc827aa8451d7190ab7
wuliushuan <wuliushuan.30044128@huawei.com>
2025-04-24 09:21:54 +0800

    FUZZ整改
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: If0cf885960bc3145f188d8e9dfc29b82cd881ad9

---
+++ test/fuzztest/normalize_service/accesstoken/revokepermissionservice_fuzzer/revokepermissionservice_fuzzer.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "revokepermissionservice_fuzzer.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <thread>
+#include <vector>
+#undef private
+#include "accesstoken_fuzzdata.h"
+#include "accesstoken_manager_service.h"
+#include "iaccess_token_manager.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+const int CONSTANTS_NUMBER_TEN = 10;
+static const int32_t ROOT_UID = 0;
+static const vector<PermissionFlag> FLAG_LIST = {
+    PERMISSION_DEFAULT_FLAG,
+    PERMISSION_USER_SET,
+    PERMISSION_USER_FIXED,
+    PERMISSION_SYSTEM_FIXED,
+    PERMISSION_GRANTED_BY_POLICY,
+    PERMISSION_COMPONENT_SET,
+    PERMISSION_POLICY_FIXED,
+    PERMISSION_ALLOW_THIS_TIME
+};
+static const uint32_t FLAG_LIST_SIZE = 8;
+
+namespace OHOS {
+    bool RevokePermissionServiceFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+
+        AccessTokenFuzzData fuzzData(data, size);
+        AccessTokenID tokenId = fuzzData.GetData<AccessTokenID>();
+        std::string testName(fuzzData.GenerateStochasticString());
+
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IAccessTokenManager::GetDescriptor());
+        uint32_t flagIndex = fuzzData.GetData<uint32_t>() % FLAG_LIST_SIZE;
+        PermissionFlag flag = FLAG_LIST[flagIndex];
+        if (!datas.WriteUint32(tokenId) || !datas.WriteString(testName) ||
+            !datas.WriteInt32(flag)) {
+            return false;
+        }
+        uint32_t code = static_cast<uint32_t>(
+            IAccessTokenManagerIpcCode::COMMAND_REVOKE_PERMISSION);
+
+        MessageParcel reply;
+        MessageOption option;
+        bool enable = ((size % CONSTANTS_NUMBER_TEN) == 0);
+        if (enable) {
+            setuid(CONSTANTS_NUMBER_TEN);
+        }
+        DelayedSingleton<AccessTokenManagerService>::GetInstance()->OnRemoteRequest(code, datas, reply, option);
+        setuid(ROOT_UID);
+
+        return true;
+    }
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::RevokePermissionServiceFuzzTest(data, size);
+    return 0;
+}
