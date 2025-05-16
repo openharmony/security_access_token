5be3aff87373326d4670c84225e622cf51fede42
wuliushuan <wuliushuan.30044128@huawei.com>
2025-05-16 17:40:06 +0800

    FUZZ NO2 整改250520
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: Ia01468b03e66a8ee7f09a97d5aeb8b193f578781

---
+++ test/fuzztest/normalize_service/accesstoken/gethaptokenidservice_fuzzer/gethaptokenidservice_fuzzer.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gethaptokenidservice_fuzzer.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <thread>
+#include <vector>
+#include "accesstoken_fuzzdata.h"
+#undef private
+#include "accesstoken_manager_service.h"
+#include "iaccess_token_manager.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+const int CONSTANTS_NUMBER_TEN = 10;
+static const int32_t ROOT_UID = 0;
+
+namespace OHOS {
+    bool GetHapTokenIDServiceFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+
+        AccessTokenFuzzData fuzzData(data, size);
+        int userID = fuzzData.GetData<int>();
+        int instIndex = static_cast<int>(size);
+        std::string bundleName(fuzzData.GenerateStochasticString());
+
+        MessageParcel sendData;
+        sendData.WriteInterfaceToken(IAccessTokenManager::GetDescriptor());
+        if (!sendData.WriteInt32(userID) || !sendData.WriteString(bundleName) || !sendData.WriteInt32(instIndex)) {
+            return false;
+        }
+
+        uint32_t code = static_cast<uint32_t>(
+            IAccessTokenManagerIpcCode::COMMAND_GET_HAP_TOKEN_I_D);
+
+        MessageParcel reply;
+        MessageOption option;
+        bool enable = ((size % CONSTANTS_NUMBER_TEN) == 0);
+        if (enable) {
+            setuid(CONSTANTS_NUMBER_TEN);
+        }
+        DelayedSingleton<AccessTokenManagerService>::GetInstance()->OnRemoteRequest(code, sendData, reply, option);
+        setuid(ROOT_UID);
+
+        return true;
+    }
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetHapTokenIDServiceFuzzTest(data, size);
+    return 0;
+}
---
+++ test/fuzztest/normalize_service/accesstoken/getnativetokeninfoservice_fuzzer/getnativetokeninfoservice_fuzzer.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getnativetokeninfoservice_fuzzer.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <thread>
+#include <vector>
+#undef private
+#include "accesstoken_fuzzdata.h"
+#include "accesstoken_kit.h"
+#include "accesstoken_manager_service.h"
+#include "iaccess_token_manager.h"
+
+using namespace std;
+using namespace OHOS::Security::AccessToken;
+const int CONSTANTS_NUMBER_TEN = 10;
+static const int32_t ROOT_UID = 0;
+static bool REAL_DATA_FLAG = true;
+
+namespace OHOS {
+    bool GetNativeTokenInfoServiceFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+        AccessTokenFuzzData fuzzData(data, size);
+        AccessTokenID tokenId = fuzzData.GetData<AccessTokenID>();
+        if (REAL_DATA_FLAG) {
+            tokenId = AccessTokenKit::GetNativeTokenId("accesstoken_service");
+        }
+        MessageParcel inData;
+        inData.WriteInterfaceToken(IAccessTokenManager::GetDescriptor());
+        if (!inData.WriteUint32(tokenId)) {
+            return false;
+        }
+
+        uint32_t code = static_cast<uint32_t>(
+            IAccessTokenManagerIpcCode::COMMAND_GET_NATIVE_TOKEN_INFO);
+
+        MessageParcel reply;
+        MessageOption option;
+        bool enable = ((size % CONSTANTS_NUMBER_TEN) == 0);
+        if (enable) {
+            setuid(CONSTANTS_NUMBER_TEN);
+        }
+        if (REAL_DATA_FLAG) {
+            setuid(ROOT_UID);
+            REAL_DATA_FLAG = false;
+        }
+        DelayedSingleton<AccessTokenManagerService>::GetInstance()->OnRemoteRequest(code, inData, reply, option);
+        setuid(ROOT_UID);
+
+        return true;
+    }
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetNativeTokenInfoServiceFuzzTest(data, size);
+    return 0;
+}
