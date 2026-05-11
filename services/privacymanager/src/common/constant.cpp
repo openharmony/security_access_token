88915d3a36832e5436c4232e411dee4ca0825fc7
zhishui001 <zhishui001@noreply.gitcode.com>
2026-05-11 12:42:42 +0800

    设备信息管理cli化新增权限
    Signed-off-by: zhishui001 <hehongyin1@h-partners.com>

--- services/privacymanager/src/common/constant.cpp
+++ services/privacymanager/src/common/constant.cpp
@@ -114,6 +114,8 @@ const std::map<std::string, int32_t> Constant::PERMISSION_OPCODE_MAP = {
         Constant::OP_ACCESS_USER_FULL_DISK_READ),
     std::map<std::string, int32_t>::value_type("ohos.permission.ACCESS_USER_FULL_DISK_WRITE",
         Constant::OP_ACCESS_USER_FULL_DISK_WRITE),
+    std::map<std::string, int32_t>::value_type("ohos.permission.cli.BUNDLE_ACTIVE_INFO",
+        Constant::OP_CLI_BUNDLE_ACTIVE_INFO),
 };
 
 bool Constant::TransferPermissionToOpcode(const std::string& permissionName, int32_t& opCode)
